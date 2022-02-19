0429f98638f567b582c93010995dca756018c0e7
Syed Ashiq <imsyedashiq@gmail.com>
2022-02-19 20:10:38 +0600

    somde modification

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
           appBarTheme: AppBarTheme(color: Color(0xFF0A0E21)),
           scaffoldBackgroundColor: Color(0xFF0A0E21)),
